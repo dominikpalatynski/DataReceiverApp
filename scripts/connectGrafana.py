a9984b72f459b975d46db9cba1b1cf2ccbd9456d
dominikpalatynski <palatynski.dominik@gmail.com>
2024-10-15 18:48:22 +0200

    Automate creating data base connections

---
+++ scripts/connectGrafana.py
@@ -0,0 +1,41 @@
+import requests
+import json
+
+# Adres URL do Grafany i API
+grafana_url = "http://localhost:3000/api/datasources"
+api_key = "glsa_9rrRRuAHZtjEoYrZxjFBHkW9QGOye2up_24d0a715"
+
+# Nagłówki uwierzytelniające i Content-Type
+headers = {
+    "Authorization": f"Bearer {api_key}",
+    "Content-Type": "application/json"
+}
+
+# Konfiguracja źródła danych dla InfluxDB 2.0 (Flux)
+data = {
+    "name": "InfluxDB",
+    "type": "influxdb",
+    "access": "proxy",
+    "url": "http://host.docker.internal:8086",  # Adres do InfluxDB
+    "user": "",  # Nie wymagane przy używaniu tokenów
+    "password": "",  # Nie wymagane przy używaniu tokenów
+    "database": "",  # Puste, bo w Flux nie definiujemy bazy danych na tym etapie
+    "jsonData": {
+        "httpMode": "GET",
+        "version": "Flux",
+        "organization": "myorg",  # Twoja organizacja w InfluxDB
+        "defaultBucket": "mybucket"  # Domyślny bucket (baza danych)
+    },
+    "secureJsonData": {
+        "token": "mytoken"  # Twój token do InfluxDB
+    }
+}
+
+# Wysłanie zapytania do API Grafany w celu utworzenia źródła danych
+response = requests.post(grafana_url, headers=headers, data=json.dumps(data))
+
+# Sprawdzenie odpowiedzi
+if response.status_code == 200:
+    print("Źródło danych zostało pomyślnie dodane.")
+else:
+    print(f"Błąd: {response.status_code}, {response.text}")
